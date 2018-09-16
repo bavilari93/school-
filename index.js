796060a2943aba598d208b2313ad6a6d0582ddf9
Bianca A <frank@Bianca-Avila-4.local>
2018-09-16 14:18:27 +0900

    lines

---
+++ index.js
@@ -0,0 +1,19 @@
+$(()=>{
+	function makeLines(num, left) {
+		console.log(num);
+  if (num > 0 & left < 450) {
+    left += 30;
+    var cm = '<div class="cm" id="line' + num + '"></div>';
+    var mm = '<div class="mm" id="mm' + num + '"></div>';
+    $('.ruler').append(cm);
+    $('.ruler').append(mm);
+    $('#line' + num).css('left', left);
+    $('#mm' + num).css('left', left + 10);
+    num--;
+    makeLines(num, left);
+  }
+}
+
+makeLines(25, 15);
+})
+
